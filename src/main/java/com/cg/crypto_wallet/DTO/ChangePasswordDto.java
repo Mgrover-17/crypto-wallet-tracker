73a85387e5407ac663d16a1f8dc332ee51c0b372
HarmanVeer24 <harmanveer0024@gmail.com>
2025-05-02 14:50:35 +0530

    [Harman] ADD: updation functionality, validation and admin role

--- src/main/java/com/cg/crypto_wallet/DTO/ChangePasswordDto.java
+++ src/main/java/com/cg/crypto_wallet/DTO/ChangePasswordDto.java
@@ -1,14 +1,35 @@
 package com.cg.crypto_wallet.DTO;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChangePasswordDto {
+    @NotBlank(message = "Email is required")
+    @Pattern(
+            regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
+            message = "Invalid email format"
+    )
+    @Email(message = "Email should be valid")
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    @Column(nullable = false)
     private String oldPassword;
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    @Column(nullable = false)
     private String newPassword;
 }
--- src/main/java/com/cg/crypto_wallet/DTO/UpdateUserDto.java
+++ src/main/java/com/cg/crypto_wallet/DTO/UpdateUserDto.java
@@ -5,19 +5,20 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @Data
-@Getter
-@Setter
 @NoArgsConstructor
-public class ResetPasswordDto {
-
+@AllArgsConstructor
+public class UpdateUserDto {
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 to 20 characters")
+    @Column(nullable = false)
+    private String username;
     @NotBlank(message = "Email is required")
     @Pattern(
             regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
@@ -26,15 +27,4 @@ public class ResetPasswordDto {
     @Email(message = "Email should be valid")
     @Column(nullable = false, unique = true)
     private String email;
-
-    @NotBlank(message = "Password is required")
-    @Size(min = 6, message = "Password must be at least 6 characters long")
-    @Column(nullable = false)
-    private String newPassword;
-    @NotBlank(message = "Password is required")
-    @Size(min = 6, message = "Password must be at least 6 characters long")
-    @Column(nullable = false)
-    private String confirmPassword;
-
-    private String otp;
 }
